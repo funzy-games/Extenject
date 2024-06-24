3057bfb294d4187b72a2fcee57839e8ea5142582
Vitalii Levytskyi <vitalii@funzy.games>
2024-06-24 22:17:25 +0300

    FUN-10784 Improve ZenUtilInternal.GetRootGameObjects() performance

--- UnityProject/Assets/Plugins/Zenject/Source/Util/ZenUtilInternal.cs
+++ UnityProject/Assets/Plugins/Zenject/Source/Util/ZenUtilInternal.cs
@@ -217,29 +217,8 @@ namespace Zenject.Internal
             using (ProfileTimers.CreateTimedBlock("Searching Hierarchy"))
 #endif
             {
-                if (scene.isLoaded)
-                {
-                    return scene.GetRootGameObjects()
-                        .Where(x => x.GetComponent<ProjectContext>() == null);
-                }
-
-                // Note: We can't use scene.GetRootObjects() here because that apparently fails with an exception
-                // about the scene not being loaded yet when executed in Awake
-                // We also can't use GameObject.FindObjectsOfType<Transform>() because that does not include inactive game objects
-                // So we use Resources.FindObjectsOfTypeAll, even though that may include prefabs.  However, our assumption here
-                // is that prefabs do not have their "scene" property set correctly so this should work
-                //
-                // It's important here that we only inject into root objects that are part of our scene, to properly support
-                // multi-scene editing features of Unity 5.x
-                //
-                // Also, even with older Unity versions, if there is an object that is marked with DontDestroyOnLoad, then it will
-                // be injected multiple times when another scene is loaded
-                //
-                // We also make sure not to inject into the project root objects which are injected by ProjectContext.
-                return Resources.FindObjectsOfTypeAll<GameObject>()
-                    .Where(x => x.transform.parent == null
-                            && x.GetComponent<ProjectContext>() == null
-                            && x.scene == scene);
+                return scene.GetRootGameObjects()
+                    .Where(x => x.GetComponent<ProjectContext>() == null);
             }
         }
 
